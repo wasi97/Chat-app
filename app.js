9e2f9c64119bcf9a91830c0808db7d3183d6e2ac
wasi97 <64901925+wasi97@users.noreply.github.com>
2020-09-19 22:12:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const msgScreen = document.getElementById("messages"); //the <ul> that displays 
 const msgForm = document.getElementById("msgform"); //the input form
 const msgInput = document.getElementById("msg-input"); //the input element to write messages
 const msgBtn = document.getElementById("msg-btn"); //the Send button
+console.log(msgBtn)
 // to store data in the msgs folder by creating a reference in database
 // const db = firebase().database();
 // const msgRef = db.ref("/msgs");
 //name of user upon loading app
 let name="dsafh";
-console.log(name)
 // function init() {
-//   name = prompt("Please enter your name");
+  //   name = prompt("Please enter your name");
 // }
 // document.addEventListener('DOMContentLoaded', init);
 
@@ -18,22 +18,22 @@ console.log(name)
 
 // send msg function to send msgs and also check that this is not empty
 function sendMessage(e){
-    e.preventDefault();
-    const text = msgInput.value;
+  e.preventDefault();
+  const text = msgInput.value;
   
-      if(!text.trim()) return alert('Please type a message'); //no msg submitted
-      const msg = {
-          name: name,
-          text: text
-      };
+  if(!text.trim()) return alert('Please type a message'); //no msg submitted
+  const msg = {
+    name: name,
+    text: text
+  };
   
-      // msgRef.push(msg);
-      firebase.database().ref("msgs").push(msg);
-      firebase.database().ref("msgs").set(msg)
-      msgInput.value = "";
-      
-    }
-    
+  // msgRef.push(msg);
+  firebase.database().ref("msgs").push(msg);
+  firebase.database().ref("msgs").set(msg)
+  msgInput.value = "";
+  
+}
+
     console.log()
 
 
