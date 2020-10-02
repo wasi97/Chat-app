51336adb6016a5cce31a8ac63d7bad22106dedd8
wasi <mw7262@gmail.com>
2020-10-03 02:49:27 +0500

    updated chat app but still confusion

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ var userDiv = document.getElementById("usersDiv")
 var loginDiv = document.getElementById("loginDiv")
 
 
+function msgRefdb(){
+    console.log(personalMsgRef)
+}
 
 function signupdetails(){
     var name = document.getElementById("name")
     userName = {username: name.value}
     // save SIGN UP data to database
     firebase.database().ref("/users").push(userName)
-    personalMsgRef = firebase.database().ref("/msgs/" + userName.username)
+    personCheck = userName.username;
+    // personalMsgRef = firebase.database().ref("/msgs/" + userName.username)
     loginDiv.classList.add("hide")
     userDiv.classList.remove("hide")
     
 };
-function 
- console.log(personalMsgRef)
 var users = document.getElementById("users")
 var chatWindow = document.getElementById("chatWindow")
 // data load from database
@@ -24,29 +26,33 @@ firebase.database().ref("/users").on("child_added",function(data){
     var li = document.createElement("li")
     var usersName = document.createTextNode(fetchedData.username)
     li.appendChild(usersName)
+    li.setAttribute("id",fetchedData.username)
+    li.setAttribute("onclick","chatbox(this)")
     users.appendChild(li)
-    users.addEventListener("click",chatbox)
-    
-    
+
 });
+   
+    
+function chatbox(e){
+    userDiv.classList.add("hide")
+    chatWindow.classList.remove("hide")
+    getuserid = e.getAttribute("id");
+}
+
 // send messages
 var msg = document.getElementById("msg")
 var msgbtn = document.getElementById("msgbtn")
 
-function chatbox(){
-    userDiv.classList.add("hide")
-    chatWindow.classList.remove("hide")
-}
 
 function sendmsg(){
-    firebase.database().ref("msgs/").push(msg.value)
+    
+    // personalMsgRef.push(msg.value)
+    firebase.database().ref("msgs/" + getuserid).push(msg.value)
     msg.value = ""
     
-}
-
-// retrieve messages from database
+    // retrieve messages from database
 var msgBox = document.getElementById("messages")
-firebase.database().ref("/msgs/").on("child_added",function(data){
+firebase.database().ref("/msgs/" + getuserid).on("child_added",function(data){
     messages = data.val();
     var li = document.createElement("li")
     var retrievedMessages = document.createTextNode(messages)
@@ -54,5 +60,6 @@ firebase.database().ref("/msgs/").on("child_added",function(data){
     msgBox.appendChild(li)
 })
 
+}
 
 
